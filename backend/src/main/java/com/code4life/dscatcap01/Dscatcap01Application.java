d7df7700a06878b28952d25fdfa34adf5cf7cce5
leninepestana <leninepestana@gmail.com>
2021-09-23 16:45:21 +0100

    Backend project created

---
+++ backend/src/main/java/com/code4life/dscatcap01/Dscatcap01Application.java
@@ -0,0 +1,13 @@
+package com.code4life.dscatcap01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dscatcap01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dscatcap01Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/code4life/dscatcap01/Dscatcap01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code4life.dscatcap01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dscatcap01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
