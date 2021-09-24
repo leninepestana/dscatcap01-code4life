00739934d71ae7d99cf8dc5e6ac6858cd6bf821f
leninepestana <leninepestana@gmail.com>
2021-09-24 01:10:52 +0100

    Adding new entry to ClientResource

--- backend/src/main/java/com/code4life/dscatcap01/resources/ClientResource.java
+++ backend/src/main/java/com/code4life/dscatcap01/resources/ClientResource.java
@@ -19,14 +19,16 @@ public class ClientResource {
 	@GetMapping
 	public ResponseEntity<List<Client>> findAll() {
 		List<Client> list = new ArrayList<>();
-		Instant instantOne = Instant.parse("1980-11-29T13:35:30.01Z");
+		Instant instantOne = Instant.parse("2010-01-19T17:35:30.01Z");
 		Instant instantTwo = Instant.parse("1975-09-26T13:35:30.01Z");
 		Instant instantThree = Instant.parse("1981-01-14T10:35:30.01Z");
 		Instant instantFour = Instant.parse("1989-06-10T10:30:30.01Z");
-		list.add(new Client(1L, "Martim Pestana", "1234567891", 7000.0, instantOne, 1));
+		Instant instantFive = Instant.parse("2007-06-10T10:30:30.01Z");
+		list.add(new Client(1L, "Martim Ferrer Silva Pestana", "1234567891", 7000.0, instantOne, 0));
 		list.add(new Client(2L, "Fernando Miguel", "5876567891", 7000.0, instantTwo, 2));
 		list.add(new Client(3L, "Ricardo Manuel dos Santos", "5581567891", 6500.0, instantThree, 0));
-		list.add(new Client(4L, "Joana Margarida Narciso", "5581567891", 6500.0, instantFour, 0));
+		list.add(new Client(4L, "Joana Margarida Narciso", "5581567891", 6200.0, instantFour, 4));
+		list.add(new Client(5L, "Daniel Alexandre Silva Antunes", "5585681891", 6000.0, instantFive, 0));
 		return ResponseEntity.ok().body(list);
 	}
 }
