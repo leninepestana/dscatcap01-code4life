2f0d4bb947de5b7943534a13e9e20eb06c5f330e
leninepestana <leninepestana@gmail.com>
2021-09-24 14:52:40 +0100

    findById without exception

--- backend/src/main/java/com/code4life/dscatcap01/services/ClientService.java
+++ backend/src/main/java/com/code4life/dscatcap01/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.code4life.dscatcap01.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,5 +31,12 @@ public class ClientService {
 		
 		return listDto;
 		*/
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.get();
+		return new ClientDTO(entity);
 	}
 }
