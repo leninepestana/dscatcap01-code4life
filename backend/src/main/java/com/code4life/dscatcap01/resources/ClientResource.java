2b5f02d7f3b7b36c0672227ad78d86d1d5391356
leninepestana <leninepestana@gmail.com>
2021-09-25 21:57:45 +0100

    Pagination

--- backend/src/main/java/com/code4life/dscatcap01/resources/ClientResource.java
+++ backend/src/main/java/com/code4life/dscatcap01/resources/ClientResource.java
@@ -1,9 +1,11 @@
 package com.code4life.dscatcap01.resources;
 
 import java.net.URI;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -26,7 +29,14 @@ public class ClientResource {
 	private ClientService service;
 	
 	@GetMapping
-	public ResponseEntity<List<ClientDTO>> findAll() {
+	public ResponseEntity<Page<ClientDTO>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+			) {
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		/*
 		List<Client> list = new ArrayList<>();		
 		Instant instantOne = Instant.parse("2010-01-19T17:35:30.01Z");
@@ -40,7 +50,7 @@ public class ClientResource {
 		list.add(new Client(4L, "Joana Margarida Narciso", "5581567891", 6200.0, instantFour, 4));
 		list.add(new Client(5L, "Daniel Alexandre Silva Antunes", "5585681891", 6000.0, instantFive, 0));
 		*/
-		List<ClientDTO> list = service.findAll();
+		Page<ClientDTO> list = service.findAllPaged(pageRequest);
 		return ResponseEntity.ok().body(list);
 	}
 
